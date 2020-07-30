228b61bb8d5a1499a10b210b4aef36aed585a697
afd <afd@alup5.academiapostal.es.local>
2020-07-30 16:23:00 +0200

    función hayGanador

---
+++ tablero.js
@@ -0,0 +1,34 @@
+/**
+ * Determina cuál es el ganador en un tablero de tres en raya (si hay alguno).
+ * Los posibles resultados son: 
+ * 0 -> no hay ganador
+ * 1 -> gana O
+ * 2 -> gana X
+ * @param {array} tablero Matriz de 3x3 números
+ */
+function hayGanador(tablero) {
+    // Comprobar filas (horizontal)
+    for (let i = 0; i < tablero.length; i++) {
+        if (tablero[i][0]==tablero[i][1] && tablero[i][0]==tablero[i][2] && tablero[i][0] != 0) {
+            return tablero[i][0];
+        }
+    }
+    // Comprobar columnas (verticales)
+    for (let n = 0; n < tablero.length; n++) { 
+        if (tablero[0][n]==tablero[1][n] && tablero[0][n]==tablero[2][n] && tablero[0][n] != 0) {
+            return tablero[0][n];
+        }
+    }
+    // Comprobar diagonales
+    if (tablero[0][0]==tablero[(1)][1] && tablero[0][0]==tablero[(2)][2]) {
+        return tablero[0][0];
+    } else if (tablero[0][2]==tablero[1][1] && tablero[0][2]==tablero[2][0]) {
+        return tablero[0][2];
+    }
+    // En caso de no ganar nadie
+    return 0;
+}
+
+module.exports = {
+    hayGanador
+}
--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 const assert = require("assert");
+const {hayGanador} = require("../tablero");
 
 describe("Determina el ganador", function() {
 
-    it ("Tablero vacío", function () {
+    it ("Tablero vacío", function() {
         assert.equal(hayGanador([
             [0, 0, 0],
             [0, 0, 0],
@@ -48,5 +49,77 @@ describe("Determina el ganador", function() {
             [2, 1, 1],
             [2, 1, 2]
         ]), 0)
+    })
+
+    it ("Horizontal de X", function () {
+        assert.equal(hayGanador([
+            [0, 1, 0],
+            [2, 2, 2],
+            [1, 1, 0]
+        ]), 2)
+    })
+
+    it ("Vertical de O", function () {
+        assert.equal(hayGanador([
+            [0, 1, 0],
+            [2, 1, 2],
+            [0, 1, 0]
+        ]), 1)
+    })
+
+    it ("Vertical de X con tablero cubierto", function () {
+        assert.equal(hayGanador([
+            [1, 1, 2],
+            [2, 1, 2],
+            [1, 2, 2]
+        ]), 2)
+    })
+
+    it ("Horizontal de O con tablero cubierto", function () {
+        assert.equal(hayGanador([
+            [1, 1, 1],
+            [2, 2, 1],
+            [1, 2, 2]
+        ]), 1)
+    })
+
+    it ("Horizontal de X con tablero cubierto", function () {
+        assert.equal(hayGanador([
+            [1, 1, 2],
+            [2, 1, 1],
+            [2, 2, 2]
+        ]), 2)
+    })
+
+    it ("Diagonal inversa de O", function () {
+        assert.equal(hayGanador([
+            [1, 2, 1],
+            [2, 1, 1],
+            [1, 2, 2]
+        ]), 1)
+    })
+
+    it ("Victoria de O sin cubrir una fila", function () {
+        assert.equal(hayGanador([
+            [0, 0, 0],
+            [1, 1, 1],
+            [2, 2, 0]
+        ]), 1)
+    })
+
+    it ("Victoria de X sin cubrir dos columnas", function () {
+        assert.equal(hayGanador([
+            [0, 1, 2],
+            [1, 0, 2],
+            [0, 1, 2]
+        ]), 2)
+    })
+
+    it ("Victoria de O con cuatro huecos iniciales", function () {
+        assert.equal(hayGanador([
+            [0, 0, 2],
+            [0, 0, 2],
+            [1, 1, 1]
+        ]), 1)
     })
 })
